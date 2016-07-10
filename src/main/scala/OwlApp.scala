7bd16d2b51925f0bee9133a7aeedb2a5903de5a3
kmehkeri <kmehkeri@gmail.com>
2016-07-10 23:01:43 +0200

    Validate that arguments are readable files

--- src/main/scala/OwlApp.scala
+++ src/main/scala/OwlApp.scala
@@ -1,11 +1,20 @@
 /**
   * Created by astaroth on 05.07.16.
   */
+
+import java.nio.file.{Paths, Files}
+
 object OwlApp {
   def main(args: Array[String]): Unit = {
-    if (args.isEmpty || args.head == "-h" || args.head == "--help")
+    // Usage
+    if (args.isEmpty || args.head == "-h" || args.head == "--help") {
       println("Usage: owl LAYOUT [INPUT1 [INPUT2 [...] ] ]")
-    else
-      println("Hello " + args.mkString(" "))
+      return
+    }
+
+    // Validate arguments
+    args foreach { file =>
+      println(file + "\t" + (if (Files.isRegularFile(Paths.get(file))) "is ok" else "does not exist or is not a readable file!"))
+    }
   }
 }
